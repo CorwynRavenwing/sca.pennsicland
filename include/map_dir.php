07fbdbb027d7a723854d6dde6915823af34cf511
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-05-10 19:05:22 -0400

    fix -1 typo in map_dir.php

--- include/map_dir.php
+++ include/map_dir.php
@@ -9,7 +9,7 @@ if (! is_dir($map_dir)) {
     ?>
 <h1 style="color:red">Note: map files missing; using last year&rsquo;s maps</h1>
     <?
-    $map_dir = "maps/${pennsic_number-1}";
+    $map_dir = "maps/" . ($pennsic_number-1);
 
     if (! is_dir($map_dir)) {
         ?>
