cf431fbe01357cbf662b48cc8bedb58fafd919e1
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-05-10 18:58:56 -0400

    error messages when maps directory is missing

--- map.php
+++ map.php
@@ -117,21 +117,7 @@ nav_right_begin();
 
 global $pennsic_number;
 
-$map_dir = "maps/${pennsic_number}";
-
-if (! is_dir($map_dir)) {
-    ?>
-<h1 style="color:red">Note: map files missing; using last year&rsquo;s maps</h1>
-    <?
-    $map_dir = "maps/${pennsic_number-1}";
-
-    if (! is_dir($map_dir)) {
-        ?>
-<h2 style="color:red">... which are also missing!</h1>
-        <?
-        $map_dir = "maps/missing";
-    }
-}
+require_once("include/map_dir.php");
 
 if (! $block) {
   // overall map page
