f99f4d2531346cf7073039f20906f5363ff9ea55
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-05-09 23:57:54 -0400

    Text and CSV files

---
+++ repeated_cooper_id_details.sql
@@ -0,0 +1,6 @@
+# Connection: pennsic
+# Host: db.sca.org
+# Saved: 2012-11-26 23:14:20
+# 
+select * from cooper_data
+where penn_number in ('3901296', '3901297', '4000090', '4100802', '4109466', '4109467', '4109470' )
