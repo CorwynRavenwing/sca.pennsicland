9a43092c3154e3a74df9bd337feeab4b4d21068e
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-06-07 14:33:27 -0400

    fix typo in nav.php

--- include/nav.php
+++ include/nav.php
@@ -90,8 +90,8 @@ function nav_head($title, $crumb_array = 0) {
   template_param("pennsic_number",  $pennsic_number  );
   template_param("title",      $title    );
 
-  $display_alias = ($alias       ? $alias              : "*NOBODY*");
-  $display_legal = ($legal_name  ? $legal_name         : "*NOBODY*");
+  $display_alias = ($alias  ? $alias              : "*NOBODY*");
+  $display_legal = ($legal_name   ? $legal_name              : "*NOBODY*");
   $display_admin = ($w_admin
                        ? "<span style='color:red; font-weight:bold'> (ADMIN)</span>"
                        : ($r_admin
@@ -99,8 +99,8 @@ function nav_head($title, $crumb_array = 0) {
                              : ""
                          )
                    );
-  $display_user  = ($user_name   ? $user_name          : "*NOBODY*");
-  $display_group = ($group_name  ? $group_name         : "<b>*NONE*</b>";
+  $display_user  = ($user_name  ? $user_name              : "*NOBODY*");
+  $display_group = ($group_name  ? $group_name              : "<b>*NONE*</b>"  );
 
   $display_group = "<b>$display_group</b>";
 
