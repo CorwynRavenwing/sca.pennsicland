50d289e62454e7471aaad02ccf103ac64d51a361
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-06-14 16:54:42 -0400

    create admin_sql

---
+++ admin_sql.php
@@ -0,0 +1,35 @@
+<?php
+# http://forums.devshed.com/mysql-help-4/create-table-php-strings-165919.html
+
+require_once("include/connect.php");
+
+/*
+$conn = mysql_connect('host','user','pass')
+    or die('Connection error');
+$database = 'databasename';
+mysql_select_db($database);
+*/
+$sql1 = 'SHOW TABLES FROM ' .$database;
+$res1 = mysql_query($sql1)
+    or die('Query 1 error:<br />' .mysql_error());
+
+echo '<table border=1 cellpadding=1 cellspacing=0 width="90%">';
+while ( $row = mysql_fetch_row($res1) )
+{
+    echo '<tr>';
+    echo '<td valign="top" align="center">' .$row[0]. '</td>';
+    echo '<td>';
+    $sql2 = 'SHOW CREATE TABLE ' .$row[0];
+    $res2 = mysql_query($sql2)
+        or die('Query 2 error:<br />' .mysql_error());
+    while ( $table_def= mysql_fetch_row($res2) )
+    {
+        for ($i=1; $i<count($table_def); $i++)
+        {
+            echo '<pre>' .$table_def[$i]. '</pre>';
+        }
+    }
+    echo '</tr>';
+}
+echo '</table>';
+?>
