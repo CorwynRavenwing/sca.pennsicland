2c27a369c24983e4c2a633cefbeb417d02661f0e
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-06-15 20:12:10 -0400

    admin_sql IGNORE code

--- admin_sql.php
+++ admin_sql.php
@@ -6,9 +6,46 @@
 <?php
 # http://forums.devshed.com/mysql-help-4/create-table-php-strings-165919.html
 
+ini_set('track_errors', 1);
+
 require_once("include/connect.php");
 
 $data_dir = "sql/";
+
+$cmd_ignore = $_GET['ignore'];
+$cmd_scan   = $_GET['scan'];
+$cmd_design = $_GET['design'];
+$cmd_check  = $_GET['check'];
+
+if ($cmd_ignore) {
+    $tablename = $cmd_ignore;
+    print "<h2>IGNORE $tablename</h2>\n";
+    $ignore_file  = $data_dir . $tablename . ".ign";
+    safe_put_contents($ignore_file, "IGNORE TABLE");
+}
+
+if ($cmd_scan) {
+    $tablename = $cmd_scan;
+    print "<h2>SCAN $tablename</h2>\n";
+    $asbuilt_file = $data_dir . $tablename . "_asbuilt.sql";
+}
+
+if ($cmd_design) {
+    $tablename = $cmd_design;
+    print "<h2>DESIGN $tablename</h2>\n";
+    $design_file  = $data_dir . $tablename . "_design.sql";
+}
+
+if ($cmd_check) {
+    $tablename = $cmd_check;
+    print "<h2>CHECK $tablename</h2>\n";
+    $asbuilt_file = $data_dir . $tablename . "_asbuilt.sql";
+    $design_file  = $data_dir . $tablename . "_design.sql";
+}
+
+
+
+
 
 $sql1 = 'SHOW TABLES FROM ' .$db_dbname;
 $res1 = mysql_query($sql1)
@@ -45,9 +82,11 @@ while ( $row = mysql_fetch_row($res1) )
     if ( (! $asbuilt_exists) and (! $design_exists) ) {
         # no files exist, ask what to do with this table
 
-        print "No files yet.\m";
+        print "No data file:\n";
         print "<a href='?ignore=$tablename'>IGNORE</a>\n";
         print "<a href='?scan=$tablename'>SCAN</a>\n";
+
+        continue;
     }
 
     print "<pre>" . get_create_sql($tablename) . "</pre>\n";
@@ -74,6 +113,23 @@ function get_create_sql($tablename) {
     }
     return implode("\n", $all_table_defs);
 } // end function get_create_sql
+
+function safe_put_contents($file, $data) {
+    global $php_errormsg;
+
+    $temp_file = $file . ".tmp";
+    if (! file_put_contents($temp_file, $data)) {
+        die("can't write to $temp_file: $php_errormsg");
+    }
+    if (file_exists($file)) {
+        if (! unlink($file)) {
+            die("can't unlink $file: $php_errormsg");
+        }
+    }
+    if (! rename($temp_file, $file)) {
+        die("can't rename $temp_file to $file: $php_errormsg");
+    }
+} // end function safe_put_contents
 ?>
 </body>
 </html>
