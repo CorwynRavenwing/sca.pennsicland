7cb443711875cec079516aa516f9cee067d6331a
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-06-20 23:04:33 -0400

    admin cooper tools

--- admin_cooper_delete_group.php
+++ admin_cooper_delete_group.php
@@ -1,7 +1,29 @@
-<html>
-<head><title>COOPER DELETE GROUP</title></head>
-<body>
+<?
+require_once("include/nav.php");
+require_once("include/cooper.php");
 
+nav_start_admin();
+
+$crumb = array(
+  "Zoning and Planning (Land)"  => "http://land.pennsicwar.org/",
+  "Land Admin"      => "admin.php",
+);
+
+nav_head( "Cooper Delete Group", $crumb );
+
+nav_admin_menu();  // special Admin menu nav
+
+nav_admin_leftnav();  // special Admin left nav
+
+nav_right_begin();
+
+if (! $r_admin) {
+  print "<h2>Please log on as Pennsic Land staff first.</h2>\n";
+} elseif (! $w_admin) {
+  print "<h2>Your access level does not allow this action.</h2>\n";
+} else {
+  # no template
+?>
 <h1>DELETE GROUP IN COOPER DATABASE</h1>
 
 <form action="http://www.cooperslake.com/prereg/land/land.php" method="post" name="coopersland" target="_self">
@@ -12,6 +34,13 @@
 	<br />
 	<input type="submit" name="submit" value="Submit">
 </form>
+<?
+} // endif admin
 
-</body>
-</html>
+nav_right_end();
+
+nav_footer_panix();
+nav_footer_disclaimer();
+
+nav_end();
+?>
