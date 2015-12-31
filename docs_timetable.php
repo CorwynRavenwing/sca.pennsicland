66c150a492cbe3013aecc3097dc1d7ff6b8a1c55
Corwyn Ravenwing <landweb@pennsicwar.org>
2015-12-31 17:07:56 -0500

    Land Agent Procedures Changes

--- docs_timetable.php
+++ docs_timetable.php
@@ -42,94 +42,108 @@ dt {font-weight:bold}
 <dl style="font-weight:normal; margin-left:0; line-height:1.3em">
 <dt>January 1
 <dd>Group registration site opens
-<dt>June 1
+<dt>June 18
 <dd>Land agents should verify all pre-registered campers with their group to find/fix errors and/or omissions
 <dt>June 23
 <dd>Final date to access/update information on the Group Land Registration Page
 <dt>July 7
 <dd>Land assignments are sent to Land Agents
-<dt>July 7 - 26
+<dt>July 7 - 29
 <dd>Land Agents negotiate group placement on their assigned block
+<dt>July 22
+<dd>Deadline for Proxy requests
 
-<dt>Fri., July 25 (noon)
+<dt>Fri., July 29 (9:00 am – 11:00 pm)
 <dd>Troll opens
-<dd>Land Agents start checking in with the Land Office (in the <span style="color:blue">Great Hall on N01</span>) to pick up land packets
+<dd>Land Agents start checking in with the Land Office (in the Great Hall on N01). Bring your completed Camping Authorization Form and Block Map, and pick up any additional information pertinent to your block. 
 <dd>Land Agents may finalize block negotiations with other Land Agents from their assigned block
+<dd>Land Agents from the same block with finalized block maps may assemble as a group and complete the Land Grab process
 
+<dt>Sat., July 30 (8:00 am - 10:00 am)
+<dd>Land Office open for LATE ARRIVAL Land Agents to check in. Bring your completed Camping Authorization Form and Block Map, and pick up any additional information pertinent to your block.
 
-<dt>Sat., July 26 (6 am - 8:45 am)
-<dd>Land Office open for LATE ARRIVAL Land Agents to pick-up land packets.
-<dd>Land Agents from same block assemble together so they can go through the Land Office process as a group.
-
-
-<dt>Sat., July 26 (9 am)
-<dd>All Land Agents who have checked in by 8:45 am must be at the <span style="color:blue">Great Hall</span>.
-<dd>Assembled groups of Land Agents from same block line up at the Land Office.
-
-<dt>Sat., July 26 (10 am)
+<dt>Sat., July 30 (10:00 am – 12:00 noon)
 <dd>Blocks with unsettled negotiations are seen by Land Office staff for dispute resolution.
 
+<dt>Sun., July 31 (10:00 am – 6:00 pm)
+<dd>Land distribution for single campers and those groups not pre-registered.
+<dd>Land Office open for assistance and questions.
 
-<dt>Sun., July 27 (9 am)
-<dd>Land distribution for those groups not pre-registered, and single campers
 </dl>
-
 
 <h2>Land Agent Procedure Summary</h2>
 <ol>
-<li>Read all information on the Land pages, and look closely at the maps since the blocks
-may have changed from last year.
-<div style=" line-height:.5" ><br></div>
-<li>Register your group.
-<div style=" line-height:.5" ><br></div>
-<li>The Land Office will e-mail Land Agents their block assignments on July 7.
-If you do not receive a notice by then, contact Land Staff.
-<div style=" line-height:.5" ><br></div>
-<li>Once you get your block assignment, contact the other Land Agents also assigned to your block.
-Try to work out which area of the block each group will camp in prior to arrival at Pennsic. This
-has worked well in the past and saves everyone time on Land Grab day.
-<div style=" line-height:.5" ><br></div>
-<li>Troll opens on Friday, July 25th at <strong>12 noon</strong>.
-No one will be allowed onto the battlefield or into the campground without going through
-Troll first. Do not arrive
-prior to <strong>noon</strong>; the staff will not be ready for you.
-<div style=" line-height:.5" ><br></div>
-<li>After checking in at Troll, Land Agents walk to the Land Office (in the <span style="color:blue">Great Hall</span>) to get your land packets.
-These will contain maps and any final instructions for completing the land process. Once you have your packet,
-you can look at your land and talk to the Land Agents from your block. The Land Office will be open Friday,
-July 25 from noon until 11:45 pm and on Saturday, July 26 from 6:00 am-8:45 am. <b>You must check in with Troll
-and the Land Office by 8:45 am on Saturday.</b>
-
-<div style=" line-height:.5" ><br></div>
-
-<li>You will be allowed to camp Friday night on the-battlefield / parking lot.
-NO camping will be allowed inside the campground, except for staff members.
-
-<div style=" line-height:.5" ><br></div>
-<li>All Land Agents must sign one block map indicating where their group is camping. You will also be required, if so noted on your block assignment email, to set aside unallocated land in your block for single campers. Single camper space must be clearly shown on the map. Single campers must have clear access to a main road.  Registered groups may not save single camper space for people arriving at a later date.
-
-<div style=" line-height:.5" ><br></div>
 
 <li>
-If you cannot come to an agreement on group placement on your block, submit your block to Land Staff for binding
-arbitration.
+	Read all information on the Land pages, and look closely at the maps since the blocks
+	may have changed from last year.
+</li>
 
-<div style=" line-height:.5" ><br></div>
+<li>
+	Register your group.
+</li>
 
-<li>Your final approved map will be used for: informational purposes; to resolve disputes; and by the Public Safety department.
+<li>
+	The Land Office will e-mail Land Agents their block assignments on July 7.
+	If you do not receive a notice by then, contact Land Staff.
+</li>
 
-<div style=" line-height:.5" ><br></div>
-<li>Once the block map has been approved, you and all your party who have gone
-through Troll will receive authorization to drive into the campground.
-<div style=" line-height:.5" ><br></div>
+<li>
+	Once you get your block assignment, contact the other Land Agents also assigned to your block.
+	Try to work out which area of the block each group will camp in prior to arrival at Pennsic. This
+	has worked well in the past and saves everyone time on Land Grab day.
+</li>
 
-<li>Rope off the perimeter of your land allocation and post your camping authorization form at the entrance in
- a waterproof container. Note: It is important that you rope off your encampment as it makes it easier for
-single campers to find unused land left in a block.
+<li>
+	Troll opens on <strong>Friday, July 29, 2016 at 9:00 am</strong>.
+	No one will be allowed onto the battlefield or into the campground without going through
+	Troll first. Do not arrive
+	prior to <strong>9:00 am</strong>; the staff will not be ready for you.
+</li>
 
+<li>
+	After checking in at Troll, Land Agents walk to the Land Office (in the Great Hall) to
+	check in with the Land Office, pick up any additional information pertinent to your block.
+	Bring your completed Camping Authorization Form and Block Map.
+	Once you have checked in with the Land Office, you may walk your land and talk to the
+	Land Agents from your block. The Land Office will be open
+	Friday, July 29, 2016 from 9:00 am until 11:00 pm and on Saturday, July 30 from 8:00 am - 6:00 pm.
+	<b>You must check in with Troll and the Land Office by 10:00 am on Saturday.</b>
+</li>
 
-<div style=" line-height:.5" ><br></div>
-<li>Once all this is finished, set up your camp and enjoy Pennsic!
+<li>
+	You will be allowed to camp Friday night on the-battlefield / parking lot.
+	NO fires are permitted on the battlefield.
+</li>
+
+<li>
+	All Land Agents must sign one block map indicating where their group is camping. You will also be required, if so noted on your block assignment email, to set aside unallocated land in your block for single campers. Single camper space must be clearly shown on the map. Single campers must have clear access to a main road.  Registered groups may not save single camper space for people arriving at a later date.
+</li>
+
+<li>
+	If you cannot come to an agreement on group placement on your block, submit your block to Land Staff for binding
+	arbitration.
+</li>
+
+<li>
+	Your final approved map will be used for: informational purposes; to resolve disputes; and by the Public Safety department.
+</li>
+
+<li>
+	Once the block map has been approved, you and all your party who have gone
+	through Troll will receive authorization to drive into the campground.
+</li>
+
+<li>
+	Rope off the perimeter of your land allocation and post your camping authorization form at the entrance in
+	 a waterproof container. Note: It is important that you rope off your encampment as it makes it easier for
+	single campers to find unused land left in a block.
+</li>
+
+<li>
+	Once all this is finished, set up your camp and enjoy Pennsic!
+</li>
+
 </ol>
 
 </div>
