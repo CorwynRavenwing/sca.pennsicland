7351fa23cdba189dc3532d0de8ed24175e5cf6d5
Corwyn Ravenwing <landweb@pennsicwar.org>
2016-06-17 21:44:11 -0400

    default to string 0 not number 0

--- sql/land_variable_design.sql
+++ sql/land_variable_design.sql
@@ -3,9 +3,9 @@ CREATE TABLE `land_variable` (
 `variable_name` varchar(50) NOT NULL default ''
 `description` varchar(255) NOT NULL default ''
 `value` varchar(255) NOT NULL default ''
-`updated` int(11) NOT NULL default 0
-`delay` int(11) NOT NULL default 0
-`queued` int(11) NOT NULL default 0
+`updated` int(11) NOT NULL default '0'
+`delay` int(11) NOT NULL default '0'
+`queued` int(11) NOT NULL default '0'
 `modified_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 PRIMARY KEY  (`variable_id`)
 KEY `VarName` (`variable_name`)
