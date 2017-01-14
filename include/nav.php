f08f02d3b0ee78c35ca1a5e9e019854aa55ec00e
Corwyn Ravenwing <landweb@pennsicwar.org>
2017-01-13 22:27:47 -0500

    add system group list to menu

--- include/nav.php
+++ include/nav.php
@@ -220,6 +220,7 @@ function nav_admin_menu($label_id = "adminmenu") {
     $count_group_compress = variable_get("count_group_compress");
     $count_group_notes    = variable_get("count_group_notes");
     $count_admin_notes    = variable_get("count_admin_notes");
+    $count_group_system   = "0";
     $count_group_kingdom  = variable_get("count_group_kingdom");
     $count_known_people   = variable_get("count_known_people");
     $count_unfixed_groups = variable_get("count_unfixed_groups");
@@ -243,6 +244,7 @@ function nav_admin_menu($label_id = "adminmenu") {
     nav_menu_active_count("w/o HISTORY",      "admin_group_nohist.php",   $count_group_nohist);
     nav_menu_active_count("w/NOTES",          "admin_notes.php",          $count_group_notes);
     nav_menu_active_count("w/NOTES (admin)",  "admin_notes_2.php",        $count_admin_notes);
+    nav_menu_active_count("w/SYSTEM FLAG",    "admin_system_group.php",   $count_group_system);
     nav_menu_active_count("w/BONUSES",        "admin_bonus.php",          $count_group_bonus);
     nav_menu_active_count("w/COMPRESS",       "admin_compress.php",       $count_group_compress);
     nav_menu_active_count("w/KINGDOMS",       "admin_kingdom.php",        $count_group_kingdom);
